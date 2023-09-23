3deba65b325a3d505cd4d2286ed710c7957848d8
mayone <mayone321@gmail.com>
2023-09-23 18:35:48 +0800

    [Change] Add chains

---
+++ app/chains/[...slug]/page.tsx
@@ -0,0 +1,79 @@
+type TokenConfig = {
+  tokenType: string
+  chainInfo: {
+    chainType: string
+    chainId: number
+  }
+  contractAddress: string
+  symbol: string
+  decimals: number
+}
+
+async function getTokens(chain: {type: string, id: number}) {
+  const res = await fetch(
+    // 'https://bridge.api.alphacarbon.network/secure/bridge/public/tokens',
+    'https://banq.api.alphacarbon.network/secure/external/tokens',
+    { cache: 'no-store' }
+  )
+  const data = await res.json() as { [symbol: string]: TokenConfig[] }
+  return Object.values(data)
+		.reduce((prev, current) => {
+			return [...prev, ...current]
+		}, [])
+		.filter((token) => token.chainInfo.chainType === chain.type && token.chainInfo.chainId === chain.id)
+}
+
+type ChainPageProps = {
+  params: {
+    slug: string[]
+  }
+}
+
+export default async function ChainPage({ params }: ChainPageProps) {
+  const chain = {type: params.slug[0], id: Number(params.slug[1])}
+  const tokens = await getTokens(chain)
+
+  return (
+    <main className="flex min-h-screen flex-col items-center p-24">
+       <div className="z-10 max-w-5xl w-full items-center justify-between text-sm lg:flex px-5 py-4">
+       <a
+          href="../"
+          className="group rounded-lg border border-transparent px-4 py-2 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+        >
+          <h4 className={`font-semibold`}>
+            <span className="inline-block transition-transform group-hover:-translate-x-1 motion-reduce:transform-none">
+              &lt;-
+            </span>
+            {' '}Back
+          </h4>
+        </a>
+      </div>
+      <div className="w-full px-5">
+        <h2 className={`mb-3 text-2xl font-semibold`}>Tokens</h2>
+      </div>
+      <div className="z-10 max-w-5xl w-full items-center justify-between text-sm lg:flex">
+        {tokens?.map((token) => {
+          return <Token key={token.tokenType} token={token} />
+        })}
+      </div>
+    </main>
+  )
+}
+
+type TokenProps = {
+  key: string
+  token: TokenConfig
+}
+
+function Token({token}: TokenProps) {
+  return (
+    <div
+      className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+    >
+      <h3 className="mb-3 text-xl font-medium">{token.tokenType}</h3>
+      <h4 className="text-sm font-mono">{token.contractAddress}</h4>
+      <h4 className="text-sm font-mono">{token.symbol}</h4>
+      <h4 className="text-sm font-mono">{token.decimals}</h4>
+    </div>
+  )
+}
---
+++ app/chains/page.tsx
@@ -0,0 +1,69 @@
+type ChainConfig = {
+  chainType: string
+  chainId: number
+  name: string
+  currencySymbol: string
+  blockExplorerUrl: string
+  rpcUrl: string
+}
+
+async function getChains() {
+  const res = await fetch(
+    // 'https://bridge.api.alphacarbon.network/secure/bridge/public/chains',
+    'https://banq.api.alphacarbon.network/secure/external/chains',
+    { cache: 'no-store' }
+  );
+  const data = await res.json()
+  return data as ChainConfig[]
+}
+
+export default async function ChainsPage() {
+  const chains = await getChains()
+
+  return (
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <div className="z-10 max-w-5xl w-full items-center justify-between text-sm lg:flex px-5 py-4">
+        <a
+          href="../"
+          className="group rounded-lg border border-transparent px-4 py-2 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+        >
+          <h4 className={`font-semibold`}>
+            <span className="inline-block transition-transform group-hover:-translate-x-1 motion-reduce:transform-none">
+              &lt;-
+            </span>
+            {' '}Back
+          </h4>
+        </a>
+      </div>
+      <div className="w-full px-5">
+        <h2 className={`mb-3 text-2xl font-semibold`}>Chains</h2>
+      </div>
+      <div className="z-10 max-w-5xl w-full items-center justify-between text-sm lg:flex">
+        {chains?.map((chain) => {
+          return <Chain key={`${chain.chainType}-${chain.chainId}`} chain={chain} />
+        })}
+      </div>
+    </main>
+  )
+}
+
+type ChainProps = {
+  key: string
+  chain: ChainConfig
+}
+
+function Chain({chain}: ChainProps) {
+  return (
+    <a
+      href={`/chains/${chain.chainType}/${chain.chainId}`}
+      className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+    >
+      <h3 className="mb-3 text-xl font-medium">{chain.name}</h3>
+      <h4 className="text-sm font-mono">{chain.chainType}</h4>
+      <h4 className="text-sm font-mono">{chain.chainId}</h4>
+      <h4 className="text-sm font-mono">{chain.currencySymbol}</h4>
+      <h4 className="text-sm font-mono">{chain.blockExplorerUrl}</h4>
+      <h4 className="text-sm font-mono">{chain.rpcUrl}</h4>
+    </a>
+  )
+}
